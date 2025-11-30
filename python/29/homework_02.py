22e80ce8c554d433d4880794927298eb5588f07a
lor2ex <koreika@gmail.com>
2025-11-30 19:49:21 +0100

    hw29

---
+++ python/29/homework_02.py
@@ -0,0 +1,55 @@
+""" 02 Генератор уникальных элементов
+
+Создайте генератор, который
+- принимает список элементов и выдаёт только уникальные значения,
+сохраняя порядок их появления в исходном списке
+
+Входные данные:
+data = [3, 1, 2, 3, 4, 1, 5, 2, 6, 7, 5, 8]
+
+Пример вывода:
+3
+1
+2
+4
+5
+6
+7
+8
+
+"""
+from typing import Generator, Any
+
+
+def generator_unique(items: list[Any]) -> Generator[Any, None, None]:
+    yield from list(dict.fromkeys(items))
+
+data = [3, 1, 2, 3, 4, 1, 5, 2, 6, 7, 5, 8]
+
+for x in generator_unique(data):
+    print(x)
+
+
+####### 2
+
+# def generator_unique(items: list[Any]) -> Generator[Any, None, None]:
+#     seen = set()
+#
+#     for item in items:
+#         if item not in seen:
+#             yield item
+#             seen.add(item)
+#
+# data = [3, 1, 2, 3, 4, 1, 5, 2, 6, 7, 5, 8]
+#
+# for x in generator_unique(data):
+#     print(x)
+
+# 3
+# 1
+# 2
+# 4
+# 5
+# 6
+# 7
+# 8
